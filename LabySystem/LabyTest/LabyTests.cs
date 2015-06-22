d36766a15eb9936817f7c24a5fb682c7653dbb37
MaxKlaxx <max@tetrisk.de>
2015-06-23 01:58:09 +0200

    ILaby - Test-Projekt hinzugefügt, close #3

---
+++ LabySystem/LabyTest/LabyTests.cs
@@ -0,0 +1,18 @@
+﻿#region # using *.*
+
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+#endregion
+
+namespace LabyTest
+{
+  [TestClass]
+  public class LabyTests
+  {
+    [TestMethod]
+    public void LabyTest()
+    {
+    }
+  }
+}
