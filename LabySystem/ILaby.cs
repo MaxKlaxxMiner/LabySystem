735a90e674382d536921e07e7966102689ae9d74
MaxKlaxx <max@tetrisk.de>
2015-06-23 02:10:12 +0200

    ILaby - ILaby Schnittstelle erstellt

--- LabySystem/ILaby.cs
+++ LabySystem/ILaby.cs
@@ -10,4 +10,34 @@ using System.Threading.Tasks;
 
 namespace LabySystem
 {
+  /// <summary>
+  /// Schnittstelle eines Labyrinth-Systems
+  /// </summary>
+  public interface ILaby
+  {
+    /// <summary>
+    /// generiert ein Teil des Labyrinthes
+    /// </summary>
+    /// <param name="ticks">Anzahl der abzuarbeitenden Rechenschritte</param>
+    /// <returns>Anzahl der noch abzuarbeitenden Rechenschritte (0 = fertig)</returns>
+    long Generate(int ticks);
+
+    /// <summary>
+    /// gibt die Breite des Spielfeldes in Pixeln zurück
+    /// </summary>
+    int Width { get; }
+
+    /// <summary>
+    /// gibt die Höhe des Spielfeldes in Pixeln zurück
+    /// </summary>
+    int Height { get; }
+
+    /// <summary>
+    /// gibt an, ob an einer bestimmten Position eine Wand ist
+    /// </summary>
+    /// <param name="x">X-Position auf dem Spielfeld</param>
+    /// <param name="y">Y-Position auf dem Spielfeld</param>
+    /// <returns>true, wenn das Spielfeld eine Wand/blockiert ist</returns>
+    bool GetWall(int x, int y);
+  }
 }
