4e09e2eba6d01260c2f229af8295f92897fa8d45
MaxKlaxx <max@tetrisk.de>
2015-07-07 04:00:42 +0200

    Windows - Scrollversion implementiert

--- LabyWindows/LabyForm.cs
+++ LabyWindows/LabyForm.cs
@@ -29,6 +29,16 @@ namespace LabyWindows
     LabyGame labyGame;
     bool labyPlayer = true;
     Bitmap labyPicture;
+    int offsetX;
+    int offsetY;
+
+    /// <summary>
+    /// minimale Kachelgröße in Pixeln (Multiplaikator von 2,2,2,2,3,5)
+    /// </summary>
+    const int minWidth = 2 * 2 * 5;
+    const int fieldWidth = 1920 / minWidth;
+    const int fieldHeight = 1080 / minWidth;
+    const int fieldJumps = 6;
 
     void DrawLaby()
     {
@@ -43,17 +53,31 @@ namespace LabyWindows
         gameGraphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.NearestNeighbor;
       }
 
-      gameGraphics.DrawImage(labyPicture, new Rectangle(0, 0, gamePicture.Width, gamePicture.Height), -0.5f, -0.5f, (float)labyPicture.Width, (float)labyPicture.Height, GraphicsUnit.Pixel);
+      if (labyPicture.Width > fieldWidth && labyPicture.Height > fieldHeight)
+      {
+        gameGraphics.DrawImage(labyPicture, new Rectangle(0, 0, gamePicture.Width, gamePicture.Height), -0.5f + offsetX, -0.5f + offsetY, fieldWidth, fieldHeight, GraphicsUnit.Pixel);
+      }
+      else
+      {
+        gameGraphics.DrawImage(labyPicture, new Rectangle(0, 0, gamePicture.Width, gamePicture.Height), -0.5f, -0.5f, (float)labyPicture.Width, (float)labyPicture.Height, GraphicsUnit.Pixel);
+      }
 
       gamePictureBox1.Refresh();
     }
 
+    #region # void InitGame() // Spielfeld initialisieren und zeichnen
+    /// <summary>
+    /// Spielfeld initialisieren und zeichnen
+    /// </summary>
     void InitGame()
     {
       if (labyGame != null) labyGame.Dispose();
       labyGame = new LabyGame(LabyGame.GetLevelSize(level).Item1, LabyGame.GetLevelSize(level).Item2, level * 1234567 * (DateTime.Now.Day + DateTime.Now.Year * 365 + DateTime.Now.Month * 372));
       labyPlayer = true;
       labyPicture = new Bitmap(labyGame.Width, labyGame.Height, PixelFormat.Format32bppRgb);
+      offsetX = 0;
+      offsetY = 0;
+      #region # // --- Spielfeld zeichnen ---
       labyGame.SetFieldChangeEvent((game, type, x, y) =>
       {
         switch (type)
@@ -71,7 +95,9 @@ namespace LabyWindows
         }
       });
       labyGame.UpdateAll();
+      #endregion
     }
+    #endregion
 
     private void LabyForm_Load(object sender, EventArgs e)
     {
@@ -91,21 +117,87 @@ namespace LabyWindows
       {
         case Keys.A:
         case Keys.NumPad4:
-        case Keys.Left: labyGame.MoveLeft(labyPlayer); break;
+        case Keys.Left:
+        {
+          labyGame.MoveLeft(labyPlayer);
+          if ((labyPlayer ? labyGame.PlayerX : labyGame.FinishX) < offsetX + fieldJumps)
+          {
+            offsetX = labyPlayer ? labyGame.PlayerX : labyGame.FinishX;
+            offsetX -= fieldWidth / 2;
+            goto case Keys.Control;
+          }
+        } break;
 
         case Keys.D:
         case Keys.NumPad6:
-        case Keys.Right: labyGame.MoveRight(labyPlayer); break;
+        case Keys.Right:
+        {
+          labyGame.MoveRight(labyPlayer);
+          if ((labyPlayer ? labyGame.PlayerX : labyGame.FinishX) >= offsetX + fieldWidth - fieldJumps)
+          {
+            offsetX = labyPlayer ? labyGame.PlayerX : labyGame.FinishX;
+            offsetX -= fieldWidth / 2;
+            goto case Keys.Control;
+          }
+        } break;
 
         case Keys.W:
         case Keys.NumPad8:
-        case Keys.Up: labyGame.MoveUp(labyPlayer); break;
+        case Keys.Up:
+        {
+          labyGame.MoveUp(labyPlayer);
+          if ((labyPlayer ? labyGame.PlayerY : labyGame.FinishY) < offsetY + fieldJumps)
+          {
+            offsetY = labyPlayer ? labyGame.PlayerY : labyGame.FinishY;
+            offsetY -= fieldHeight / 2;
+            goto case Keys.Control;
+          }
+        } break;
 
         case Keys.S:
         case Keys.NumPad2:
-        case Keys.Down: labyGame.MoveDown(labyPlayer); break;
+        case Keys.Down:
+        {
+          labyGame.MoveDown(labyPlayer);
+          if ((labyPlayer ? labyGame.PlayerY : labyGame.FinishY) >= offsetY + fieldHeight - fieldJumps)
+          {
+            offsetY = labyPlayer ? labyGame.PlayerY : labyGame.FinishY;
+            offsetY -= fieldHeight / 2;
+            goto case Keys.Control;
+          }
+        } break;
 
         case Keys.Space: labyPlayer = !labyPlayer; break;
+
+        case Keys.Enter:
+        {
+          if (labyPicture.Width > fieldWidth && labyPicture.Height > fieldHeight)
+          {
+            if (labyPlayer)
+            {
+              offsetX = labyGame.PlayerX;
+              offsetY = labyGame.PlayerY;
+            }
+            else
+            {
+              offsetX = labyGame.FinishX;
+              offsetY = labyGame.FinishY;
+            }
+            offsetX -= fieldWidth / 2;
+            offsetY -= fieldHeight / 2;
+          }
+          goto case Keys.Control;
+        }
+        case Keys.Control:
+        {
+          if (labyPicture.Width > fieldWidth && labyPicture.Height > fieldHeight)
+          {
+            if (offsetX < 0) offsetX = 0;
+            if (offsetY < 0) offsetY = 0;
+            if (offsetX > labyPicture.Width - fieldWidth) offsetX = labyPicture.Width - fieldWidth;
+            if (offsetY > labyPicture.Height - fieldHeight) offsetY = labyPicture.Height - fieldHeight;
+          }
+        } break;
 
         case Keys.Escape: Close(); break;
       }
